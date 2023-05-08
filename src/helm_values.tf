2da7eee0aad9bd632c86156f997cc6a0f94f430c
chrisghill <chris@massdriver.cloud>
2023-05-08 00:52:43 -0600

    first commit

---
+++ src/helm_values.tf
@@ -0,0 +1,80 @@
+locals {
+  bucket_name = element(split(":", var.bucket.data.infrastructure.arn), length(split(":", var.bucket.data.infrastructure.arn)) - 1)
+
+  loki_values = {
+    singleBinary = {
+      replicas = var.loki.scalable ? 0 : lookup(var.loki.singleBinary, "replicas", null)
+    }
+    backend = {
+      replicas = var.loki.scalable ? lookup(var.loki.backend, "replicas", null) : null
+    }
+    write = {
+      replicas = var.loki.scalable ? lookup(var.loki.write, "replicas", null) : null
+    }
+    read = {
+      replicas = var.loki.scalable ? lookup(var.loki.read, "replicas", null) : null
+    }
+    serviceAccount = {
+      name = var.md_metadata.name_prefix
+      annotations = {
+        "eks.amazonaws.com/role-arn" = module.application.id
+      }
+    }
+    loki = {
+      auth_enabled = false
+      podLabels    = var.md_metadata.default_tags
+      storage = {
+        type = "s3"
+        s3 = {
+          region = var.bucket.specs.aws.region
+        }
+        bucketNames = {
+          chunks = local.bucket_name
+          ruler  = local.bucket_name
+          admin  = local.bucket_name
+        }
+      }
+    }
+    test = {
+      enabled = false
+    }
+    monitoring = {
+      lokiCanary = {
+        enabled = false
+      }
+    }
+  }
+
+  promtail_values = {
+    podLabels = var.md_metadata.default_tags
+    config = {
+      clients = [{
+        url = "http://loki-gateway.${var.namespace}.svc.cluster.local/loki/api/v1/push"
+      }]
+    }
+  }
+
+  grafana_values = {
+    extraLabels = var.md_metadata.default_tags
+    adminPassword = module.application.secrets.grafanaAdminPassword
+    datasources = {
+      "loki.yaml" = {
+        apiVersion : 1
+        datasources : [{
+          name : "Loki"
+          type : "loki"
+          access : "proxy"
+          url : "http://loki-gateway.${var.namespace}.svc.cluster.local"
+          version : 1
+          isDefault : true
+          jsonData : {}
+        }]
+      }
+    }
+    persistence = {
+      enabled = true
+      type    = "StatefulSet"
+    }
+    useStatefulSet = true
+  }
+}
---
+++ src/main.tf
@@ -0,0 +1,61 @@
+locals {
+  loki_release     = "${var.md_metadata.name_prefix}-loki"
+  promtail_release = "${var.md_metadata.name_prefix}-promtail"
+  grafana_release  = "${var.md_metadata.name_prefix}-grafana"
+
+  loki_version     = "5.5.0"
+  promtail_version = "6.11.0"
+  grafana_version  = "6.56.1"
+}
+
+module "application" {
+  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application?ref=87cc8c2"
+  name    = var.md_metadata.name_prefix
+  service = "kubernetes"
+
+  kubernetes = {
+    namespace        = var.namespace
+    cluster_artifact = var.kubernetes_cluster
+  }
+}
+
+resource "helm_release" "loki" {
+  name             = local.loki_release
+  chart            = "loki"
+  repository       = "https://grafana.github.io/helm-charts"
+  version          = local.loki_version
+  namespace        = var.namespace
+  create_namespace = true
+
+  values = [
+    yamlencode(local.loki_values)
+  ]
+}
+
+resource "helm_release" "promtail" {
+  count            = var.promtail.enabled ? 1 : 0
+  name             = local.promtail_release
+  chart            = "promtail"
+  repository       = "https://grafana.github.io/helm-charts"
+  version          = local.promtail_version
+  namespace        = var.namespace
+  create_namespace = true
+
+  values = [
+    yamlencode(local.promtail_values)
+  ]
+}
+
+resource "helm_release" "grafana" {
+  count            = var.grafana.enabled ? 1 : 0
+  name             = local.grafana_release
+  chart            = "grafana"
+  repository       = "https://grafana.github.io/helm-charts"
+  version          = local.grafana_version
+  namespace        = var.namespace
+  create_namespace = true
+
+  values = [
+    yamlencode(local.grafana_values)
+  ]
+}
